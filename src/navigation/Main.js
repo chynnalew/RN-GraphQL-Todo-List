9c8dc2a6c3600f5c0213515d4d63a3a325ca7797
Chynna Lew <chynnalew@yahoo.com>
2022-02-01 12:03:08 -0800

    update Mainjs to link to apollo client

--- src/navigation/Main.js
+++ src/navigation/Main.js
@@ -2,23 +2,37 @@ import React from 'react';
 import { AsyncStorage } from 'react-native';
 import Drawer from './DrawerNavigator';
 import CenterSpinner from '../screens/components/Util/CenterSpinner';
-
-console.disableYellowBox = true;
+import { ApolloProvider } from 'react-apollo';
+import makeApolloClient from '../apollo';
 
 const Main = () => {
+
+  const [client, setClient] = React.useState(null);
 
   const fetchSession = async () => {
     // fetch session
     const session = await AsyncStorage.getItem('@todo-graphql:session');
     const sessionObj = JSON.parse(session);
     const { token, id } = sessionObj;
+
+    const client = makeApolloClient(token);
+
+    setClient(client);
   }
 
   React.useEffect(() => {
     fetchSession();
   }, [])
 
-  return <Drawer />
+  if (!client) {
+    return <CenterSpinner />
+  }
+
+  return (
+    <ApolloProvider client={client}>
+      <Drawer />
+    </ApolloProvider>
+  );
 }
 
 export default Main;
