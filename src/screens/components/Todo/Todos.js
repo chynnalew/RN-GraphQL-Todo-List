ece0a97c2e54b230170c283f9dd1e94f4054379b
Chynna Lew <chynnalew@yahoo.com>
2022-02-01 13:14:53 -0800

    create query with variables

--- src/screens/components/Todo/Todos.js
+++ src/screens/components/Todo/Todos.js
@@ -10,27 +10,28 @@ import gql from 'graphql-tag';
 import TodoItem from './TodoItem';
 import LoadOlder from './LoadOlder';
 import LoadNewer from './LoadNewer';
-import {useQuery} from '@apollo/react-hooks';
+import {useQuery} from 'react-apollo';
 import CenterSpinner from '../Util/CenterSpinner';
 
 export const FETCH_TODOS = gql`
-query ($isPublic: Boolean) {
-  todos(
-    order_by: {
-      created_at: desc
-    },
-    where: {is_public: {_eq: $isPublic}}
-  ) {
-    id
-    title
-    is_completed
-    created_at
-    is_public
-    user{
-      name
-    }
-  }
-}`
+ query {
+   todos (
+     order_by: {
+       created_at: desc
+     },
+     where: { is_public: { _eq: false} }
+   ) {
+     id
+     title
+     is_completed
+     created_at
+     is_public
+     user {
+       name
+     }
+   }
+ }
+`;
 
 const Todos = ({ isPublic, ...props }) => {
 
@@ -60,6 +61,14 @@ const Todos = ({ isPublic, ...props }) => {
   //     }
   //   ]
   // }
+  const { data, error, loading} = useQuery(
+    FETCH_TODOS,
+    {
+      variables: {isPublic}
+    }
+  );
+
+  if(!data) return null;
 
   return (
     <View style={styles.container}>
