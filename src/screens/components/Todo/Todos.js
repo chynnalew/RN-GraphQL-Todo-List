45cc8f0f49235e982bb9645e8c7282e8f019d5ef
Chynna Lew <chynnalew@yahoo.com>
2022-02-01 13:23:28 -0800

    add query error handling

--- src/screens/components/Todo/Todos.js
+++ src/screens/components/Todo/Todos.js
@@ -14,79 +14,61 @@ import {useQuery} from 'react-apollo';
 import CenterSpinner from '../Util/CenterSpinner';
 
 export const FETCH_TODOS = gql`
- query {
-   todos (
-     order_by: {
-       created_at: desc
-     },
-     where: { is_public: { _eq: false} }
-   ) {
-     id
-     title
-     is_completed
-     created_at
-     is_public
-     user {
-       name
-     }
-   }
- }
+query ($isPublic: Boolean) {
+  todos (
+    order_by: {
+      created_at: desc
+    },
+    where: { is_public: { _eq: $isPublic} }
+  ) {
+    id
+    title
+    is_completed
+      created_at
+    is_public
+    user {
+      name
+    }
+  }
+}
 `;
 
 const Todos = ({ isPublic, ...props }) => {
 
   const [newTodosExist, setNewTodosExist] = React.useState(true);
 
-  // const data = {
-  //   todos: [
-  //     {
-  //       id: "1",
-  //       title: "This is todo 1",
-  //       is_completed: true,
-  //       is_public: isPublic,
-  //       user: {
-  //         id: "1",
-  //         name: "user1"
-  //       }
-  //     },
-  //     {
-  //       id: "2",
-  //       title: "This is todo 2",
-  //       is_completed: false,
-  //       is_public: isPublic,
-  //       user: {
-  //         id: "2",
-  //         name: "user2"
-  //       }
-  //     }
-  //   ]
-  // }
-  const { data, error, loading} = useQuery(
+  const { data, error, loading } = useQuery(
     FETCH_TODOS,
     {
-      variables: {isPublic}
+      variables: { isPublic }
     }
   );
 
-  if(!data) return null;
-
+  if (error) {
+    console.error(error);
+    return <Text>Error</Text>;
+  }
+  
+  if (loading) {
+    return <CenterSpinner />;
+  }
+  
   return (
     <View style={styles.container}>
-      <LoadNewer show={isPublic} styles={styles} isPublic={isPublic}/>
+      <LoadNewer show={this.state.newTodosExist && isPublic} toggleShow={this.dismissNewTodoBanner} styles={styles} isPublic={this.props.isPublic}/>
       <ScrollView style={styles.scrollView} contentContainerStyle={styles.scrollViewContainer}>
         <FlatList
           data={data.todos}
-          renderItem={({item}) => <TodoItem item={item} isPublic={isPublic}/>}
+          renderItem={({item}) => <TodoItem item={item} isPublic={this.props.isPublic}/>}
           keyExtractor={(item) => item.id.toString()}
         />
         <LoadOlder
-          isPublic={isPublic}
+          isPublic={this.props.isPublic}
           styles={styles}
         />
       </ScrollView>
     </View>
-  );
-
+  )
 }
 
 const styles = StyleSheet.create({
